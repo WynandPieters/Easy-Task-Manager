7c7c529eba6bc34bc8301bb97be8433c820db230
Wynand <wynandp@lemonkode.com>
2024-06-14 15:44:21 +0200

    Added comments to explain signals.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
   selector: 'app-user',
@@ -9,6 +10,10 @@ import { Component, Input } from '@angular/core';
 export class UserComponent {
   @Input() avatar!: string;
   @Input() name!: string;
+
+  /** SIGNAL INPUTS */
+  // selectedUser = signal((DUMMY_USERS[randomIndex])) **If randomIndex was a global variable assigned = Math.floor(Math.random() * DUMMY_USERS.length)
+  // imagePath = computed(() => 'assets/users/' + this.selectedUsers().avatar) **Use instead of get imagePath(){}
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
