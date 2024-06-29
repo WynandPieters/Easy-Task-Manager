04780f8bd899846402c0950fe352bc97b25ff90f
WynandPieters <wynandp@gmail.com>
2024-06-29 19:15:01 +0200

    Created a Task Manager app.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,23 +1,28 @@
-import { Component, Input } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [CardComponent]
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({ required: true }) users!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
 
   /** SIGNAL INPUTS */
   // selectedUser = signal((DUMMY_USERS[randomIndex])) **If randomIndex was a global variable assigned = Math.floor(Math.random() * DUMMY_USERS.length)
   // imagePath = computed(() => 'assets/users/' + this.selectedUsers().avatar) **Use instead of get imagePath(){}
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.users.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.users.id);
+  }
 }
